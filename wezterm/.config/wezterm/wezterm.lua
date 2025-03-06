dbdbe3a6ceeba3b2b97ec7d67a489d8e752f2a7a
Tom Fry <tom.mfry@gmail.com>
2025-03-06 12:41:04 +0000

    update to waybar modules and styling

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,6 +6,6 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Mocha"
 config.font_size = 12
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.95
+config.window_background_opacity = 0.85
 
 return config
